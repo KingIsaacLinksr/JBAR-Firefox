1056c24f9b63897ce993e09e54fc529fc536d8d8
KingIsaacLinksr <kingisaaclinksr@gmail.com>
2013-10-21 23:56:37 -0700

    Update geturl.js

--- chrome/content/geturl.js
+++ chrome/content/geturl.js
@@ -7,95 +7,27 @@
                         amazon : {
                             rx: /^http.*?\.amazon\.com.*?(\/dp\/|\/o\/asin\/|\/exec\/obidos\/tg\/detail\/|\/gp\/product\/)/i,
                             params: [
-                                { param: "tag", paramValue: "thelinactsho-20" }
+                                { param: "tag", paramValue: "tekno09-20" }
                             ]
                         },
                         amazonuk : {
                             rx: /^http.*?\.amazon\.co\.uk.*?(\/dp\/|\/o\/asin\/|\/exec\/obidos\/tg\/detail\/|\/gp\/product\/)/i,
                             params: [
-                                { param: "tag", paramValue: "jupitebroadc-21" }
+                                { param: "tag", paramValue: "tekno-21" }
                             ]
                         },
                         amazonde : { 
                             rx: /^http.*?\.amazon\.de.*?(\/dp\/|\/o\/asin\/|\/exec\/obidos\/tg\/detail\/|\/gp\/product\/)/i,
                             params: [
-                                { param: "tag", paramValue: "jupitebroad02-21" }
+                                { param: "tag", paramValue: "tekno03-21" }
                             ]
                         },
-                        amazonca : { 
-                            rx: /^http.*?\.amazon\.ca.*?(\/dp\/|\/o\/asin\/|\/exec\/obidos\/tg\/detail\/|\/gp\/product\/)/i,
+                        amazonfr : { 
+                            rx: /^http.*?\.amazon\.fr.*?(\/dp\/|\/o\/asin\/|\/exec\/obidos\/tg\/detail\/|\/gp\/product\/)/i,
                             params: [
-                                { param: "tag", paramValue: "jbcanada-20" }
+                                { param: "tag", paramValue: "tekno0b-21" }
                             ]
                         },
-                        audible : {
-                            rx: /^http.*?\.audible\.com.*?\/pd\/.*/i,
-                            params: [
-                                { param: "source_code", paramValue: "COMA0230WS012110" },
-                                { param: "AID", paramValue: "10298646" },
-                                { param: "PID", paramValue: "4897915" }
-                            ]
-                        },
-                        mint : {
-                            rx: /^http.*?\.mint\.com/i,
-                            params: [
-                                { param: "PID", paramValue: "4897915" },
-                                { param: "priorityCode", paramValue: "4216102399" },
-                                { param: "source", paramValue: "cj_pfm" }
-                            ]
-                        },
-                        bestbuy : {
-                            rx: /^http.*?\.bestbuy\.com.*?site.*?\?id=/i, 
-                            params: [
-                                { param: "AID", paramValue: "10483113" },
-                                { param: "PID", paramValue: "4897915" },
-                                { param: "ref", paramValue: "39" },
-                                { param: "CJPID", paramValue: "4897915" },
-                                { param: "loc", paramValue: "01" }
-                            ]
-                        },
-                        thinkgeek : {
-                            rx: /^http.*?\.thinkgeek\.com\/product\//i,
-                            params: [
-                                { param: "cpg", paramValue: "cj" },
-                                { param: "ref", paramValue: "" },
-                                { param: "CJURL", paramValue: "" },
-                                { param: "CJID", paramValue: "3282554"}
-                            ]
-                        },
-                        neweggcom : {
-                            rx: /^http.*?\.newegg\.com.*?(product.product\.aspx\?item=|special.shellshocker\.aspx\?)/i,
-                            params: [
-                                { param: "nm_mc", paramValue: "AFC-C8Junction" },
-                                { param: "cm_mmc", paramValue: "AFC-C8Junction-_-Branding-_-na-_-na" },
-                                { param: "AID", paramValue: "10440554" },
-                                { param: "PID", paramValue: "4897915" }
-                            ]
-                        },
-                        neweggca : {
-                            rx: /^http.*?\.newegg\.ca.*?(product.product\.aspx\?item=|special.shellshocker\.aspx\?)/i,
-                            params: [
-                                { param: "nm_mc", paramValue: "AFC-C8junctionCA" },
-                                { param: "cm_mmc", paramValue: "AFC-C8JunctionCA-_-homepage-_-na-_-na" },
-                                { param: "AID", paramValue: "10606701" },
-                                { param: "PID", paramValue: "4897915" }
-                            ]
-                        },
-                        woot : {
-                           rx: /^http.*?\.woot\.com.*?\/offers\/.*/i,
-                           params: [
-                                { param: "utm_campaign", paramValue: "Commission+Junction+-+10848750" },
-                                { param: "utm_source", paramValue: "Commission+Junction+Publisher+-+4897915" },
-                                { param: "utm_medium", paramValue: "affiliate+-+Woot%21+Logo" }
-                           ]
-                        },
-                        guitarcenter : {
-                            rx: /^http.*?\.guitarcenter\.com\/.*[a-z0-9]{9}-i[a-z0-9]{7}\.gc/i,
-                            params: [
-                                { param: "CJAID", paramValue: "10453836" },
-                                { param: "CJPID", paramValue: "4897915" }
-                            ]
-                        }
                     };
 
                 function createTag(params) {
